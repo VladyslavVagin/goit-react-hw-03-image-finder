1c806cd06de19f12cd9e5d2761f7741665071139
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-12-02 22:25:11 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import { Component } from "react"
-import SearchBar from "./SearchBar/SearchBar"
-import ImageGallery from "./ImageGallery/ImageGallery"
-import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem"
-import getPicturesApi from "./api-request/api-request"
-import Button from "./Button/Button"
+import { Component } from 'react';
+import SearchBar from './SearchBar/SearchBar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+import getPicturesApi from './api-request/api-request';
+import Button from './Button/Button';
 
 export class App extends Component {
   state = {
@@ -11,37 +11,40 @@ export class App extends Component {
     isLoading: false,
     page: 1,
     searchData: '',
-  }
+  };
 
-  
+  onSubmitForm = event => {
+    event.preventDefault();
+    const searchData = event.target.elements[1].value;
+    this.setState({ searchData });
+    if (searchData.trim() !== '') {
+      getPicturesApi(searchData, this.state.page)
+        .then(r => r.data)
+        .then(data => {
+          if (data.total === 0) {
+            Promise.reject(new Error('Images not founded, we so sorry'));
+          }
+          this.setState({ images: data.hits });
+        })
+        .catch(error => console.log(error.message))
+        .finally(() => event.target.elements[1].value = '');
+    } else {
+      alert('Please write your request correctly!!!');
+    }
+  };
 
-  onSubmitForm = (event) => {
-     event.preventDefault();
-     const searchData = event.target.elements[1].value;
-     if (searchData.trim() !== '') {
-      this.setState({searchData});
-      getPicturesApi(searchData, this.state.page).then(r => r.data).then(data => {
-        if(data.total === 0) {
-          Promise.reject(new Error('Images not founded, we so sorry'));
-        }
-        this.setState({images: data.hits});
-      }).catch(error => console.log(error.message)).finally(() => event.target.elements[1].value = '');
-     } else {
-      alert('Please write your request correctly!!!')
-     }
-  }
-
-  onClick= () => this.setState(({page}) => ({page: page + 1}));
-
+  onClick = () => this.setState(({ page }) => ({ page: page + 1 }));
 
   render() {
-    const {images} = this.state;
-    return <div>
-      <SearchBar onSubmit={this.onSubmitForm} />
-      <ImageGallery>
-         <ImageGalleryItem images={images}/>
-      </ImageGallery>
-      <Button onClick={this.onClick}/>
-    </div>
-  } 
-};
+    const { images } = this.state;
+    return (
+      <div>
+        <SearchBar onSubmit={this.onSubmitForm} />
+        <ImageGallery>
+          <ImageGalleryItem images={images} />
+        </ImageGallery>
+       <Button onClick={this.onClick} />
+      </div>
+    );
+  }
+}
