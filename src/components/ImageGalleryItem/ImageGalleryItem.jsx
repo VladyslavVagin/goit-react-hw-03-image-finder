0b7167adc37d7c91da7353087478887c8dcab72a
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-12-06 21:27:37 +0100

    update

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
-import css from './ImageGalleryItem.module.css'
+import css from './ImageGalleryItem.module.css';
 import Modal from 'components/Modal/Modal';
 
 export default class ImageGalleryItem extends Component {
   state = {
     showModal: false,
-  }
+  };
 
   toggleModal = () => {
-    if(this.state.showModal) {
-      this.setState({showModal: false});
+    if (this.state.showModal) {
+      this.setState({ showModal: false });
     } else {
-      this.setState({showModal: true});
+      this.setState({ showModal: true });
     }
-  }
+  };
 
-
-
-   render() {
-    const {showModal} = this.state;
+  render() {
+    const { showModal } = this.state;
     const { image } = this.props;
-    const { webformatURL, tags, largeImageURL} = image;
-    return ( 
+    const { webformatURL, tags, largeImageURL } = image;
+    return (
       <>
-        {showModal && <Modal onClose={this.toggleModal} picture={largeImageURL} tags={tags} />}
-              <li  className={css.item} >
-              <img src={webformatURL} alt={tags} loading='lazy' className={css.image}/>
-            </li>
-            </>
-            )
-   }
-   }
+        {showModal && (
+          <Modal
+            onClose={this.toggleModal}
+            picture={largeImageURL}
+            tags={tags}
+          />
+        )}
+        <li className={css.item} onClick={this.toggleModal}>
+          <img
+            src={webformatURL}
+            alt={tags}
+            loading="lazy"
+            className={css.image}
+          />
+        </li>
+      </>
+    );
+  }
+}
